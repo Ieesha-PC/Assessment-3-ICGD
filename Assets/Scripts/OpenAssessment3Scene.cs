65f952da43f7794d05a240ee57b51e438776cca2
Ieesha-PC <ieesha.abdallah@student.uts.edu.au>
2023-11-03 15:08:52 +1100

    s

---
+++ Assets/Scripts/OpenAssessment3Scene.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenAssessment3Scene : MonoBehaviour
+{
+    public void LoadScene(string Assessment3Scene)
+    {
+        SceneManager.LoadScene(Assessment3Scene);
+    }
+}
